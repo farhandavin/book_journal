696612a69c3666b55274e72e2f9e90fec971a006
FarhanDavinRinaldi <farhandavin14@gmail.com>
2026-06-17 15:48:48 +0700

    fix: change borrowings.index route to my.books in navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,7 +9,7 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('borrowings.index')" :active="request()->routeIs('borrowings.*')" class="text-gray-600 hover:text-black">
+                    <x-nav-link :href="route('my.books')" :active="request()->routeIs('my.books')" class="text-gray-600 hover:text-black">
                         {{ __('Peminjaman buku saya') }}
                     </x-nav-link>
 
@@ -54,7 +54,7 @@
 
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden border-t border-gray-200">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('borrowings.index')" :active="request()->routeIs('borrowings.*')">
+            <x-responsive-nav-link :href="route('my.books')" :active="request()->routeIs('my.books')">
                 {{ __('Peminjaman buku saya') }}
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
